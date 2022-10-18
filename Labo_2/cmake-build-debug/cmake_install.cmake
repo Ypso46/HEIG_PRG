163825954f7f26a66e24de4ec8b66ebe0953f1e5
Ypso46 <gd.temp@pm.me>
2022-10-18 15:18:30 +0200

    test

--- Labo_2/cmake-build-debug/cmake_install.cmake
+++ Labo_2/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Greg/Documents/PRG/Semestre_1/Labo_2
+# Install script for directory: C:/Users/Greg/Dev/HEIG_PRG/Labo_2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/untitled1")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Labo_2")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Greg/Documents/PRG/Semestre_1/Labo_2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Greg/Dev/HEIG_PRG/Labo_2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- Labo_2/main.cpp
+++ Labo_2/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <cstdlib>
 
 using namespace std;
 
@@ -8,68 +9,104 @@ int main() {
     int supValue = 0;
     int userValue = 0;
 
-    cout << "Enter the inferior value superior to 0" << endl;
-    cin >> infValue;
-
-    while (infValue < 0) {
-        cout << "Wrong value, please choose again: " << endl;
-        cin >> infValue;
-    }
-
-    cout << "Enter the superior value:" << endl;
-    cin >> supValue;
-
-    while (supValue < infValue) {
-        cout << "Wrong value, please choose again:" << endl;
-        cin >> supValue;
-    }
-
-    cout << "Enter a value between your inferior and superior values" << endl;
-    cin >> userValue;
-
-    while (userValue <= infValue || userValue >= supValue) {
-        cout << "Wrong value, please choose again: " << endl;
-        cin >> userValue;
-    }
-
-    cout << "Your inferior value is: " << infValue << endl;
-    cout << "Your superior value is: " << supValue << endl;
-    cout << "The number to guess is: " << userValue << endl << endl;
+    char gameOrTest;
 
 
-    char userChoice = 0;
-    int guessRange = (supValue + infValue) / 2;
-
-    cout << "Answer the questions with 'y' for yes and 'n' for no." << endl;
-
-    while (infValue != userValue || supValue != userValue) {
-        cout << "Is your number between: " << ((infValue + supValue) / 2) << " and " << supValue << endl;
-        cin >> userChoice;
-
-        if (userChoice == 'y') {
-            infValue = guessRange;
-        } else if (userChoice == 'n') {
-            supValue = guessRange;
-        } else {
-            cout << "Error, choose 'y' or 'n'" << endl;
-        }
-    }
-
-    /*
-    char userInput;
-
-    string choicePhrase =  "Game mode or test mode? Enter 'g' for game mode and 't' for test)";
+    string choicePhrase =  "Enter 'g' for game mode and 't' for test)";
 
     cout << choicePhrase << endl;
-    cin >> userInput;
+    cin >> gameOrTest;
 
-    if (userInput == 'g') {
+    if (gameOrTest == 'g') {
         cout << "Game mode" << endl;
-    } else if (userInput == 't') {
+
+        cout << "Enter the inferior value superior to 0" << endl;
+        cin >> infValue;
+
+        while (infValue < 0) {
+            cout << "Wrong value, please choose again: " << endl;
+            cin >> infValue;
+        }
+
+        cout << "Enter the superior value:" << endl;
+        cin >> supValue;
+
+        while (supValue < infValue) {
+            cout << "Wrong value, please choose again:" << endl;
+            cin >> supValue;
+        }
+
+        cout << "Your inferior value is: " << infValue << endl;
+        cout << "Your superior value is: " << supValue << endl;
+
+        char userChoice = 0;
+
+        cout << "Choose a number in this range and answer the questions with 'y' for yes and 'n' for no." << endl;
+
+        while (infValue != supValue ) {
+            int guess = (supValue + infValue) / 2;
+            cout << "Is your number smaller or equal to " << guess << endl;
+            cin >> userChoice;
+
+            if (userChoice == 'y') {
+                supValue = guess;
+            } else if (userChoice == 'n') {
+                infValue = guess + 1;
+            } else {
+                cout << "Error, choose 'y' or 'n'" << endl;
+            }
+        }
+        cout << "Your number is: " << infValue << endl;
+
+    } else if (gameOrTest == 't') {
         cout << "Test mode" << endl;
+
+        cout << "Enter the inferior value superior to 0" << endl;
+        cin >> infValue;
+
+        while (infValue < 0) {
+            cout << "Wrong value, please choose again: " << endl;
+            cin >> infValue;
+        }
+
+        cout << "Enter the superior value:" << endl;
+        cin >> supValue;
+
+        while (supValue < infValue) {
+            cout << "Wrong value, please choose again:" << endl;
+            cin >> supValue;
+        }
+
+        cout << "Your inferior value is: " << infValue << endl;
+        cout << "Your superior value is: " << supValue << endl;
+
+        srand(time(NULL));
+        int randomNumber = rand() % (infValue-supValue) + 1;
+        cout << "Your random number is: " << randomNumber << endl;
+
+
+        char userChoice = 0;
+
+        cout << "Choose a number in this range and answer the questions with 'y' for yes and 'n' for no." << endl;
+
+        while (infValue != supValue ) {
+            int guess = (supValue + infValue) / 2;
+            cout << "Is your number smaller or equal to " << guess << endl;
+            cin >> userChoice;
+
+            if (userChoice == 'y') {
+                supValue = guess;
+            } else if (userChoice == 'n') {
+                infValue = guess + 1;
+            } else {
+                cout << "Error, choose 'y' or 'n'" << endl;
+            }
+        }
+        cout << "Your number is: " << infValue << endl;
+
     } else {
         cout << "Error!" << endl;
     }
-    */
+
     return 0;
 }
