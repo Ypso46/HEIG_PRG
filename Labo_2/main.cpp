dea7916135bbb77a22d19ad309ca8af4a2ca2207
Ypso46 <gd.temp@pm.me>
2022-10-19 16:41:13 +0200

    last features

--- Labo_2/main.cpp
+++ Labo_2/main.cpp
@@ -8,8 +8,11 @@ int main() {
     int infValue = 0;
     int supValue = 0;
     int guessNumber = 0;
+    
+
     char userChoice = 0;
     char gameOrTest;
+
     string yesOrNo = "Answer the questions with 'y' for yes and 'n' for no.";
     string modeChoice =  "Enter 'g' for game mode and 't' for test)";
 
@@ -17,9 +20,10 @@ int main() {
     cin >> gameOrTest;
 
     if (gameOrTest == 'g') {
+        
         cout << "Game mode" << endl;
 
-        cout << "Enter the inferior value superior to 0" << endl;
+        cout << "Enter the inferior value superior or equal to 0" << endl;
         cin >> infValue;
 
         while (infValue < 0) {
@@ -41,14 +45,14 @@ int main() {
         cout << "Choose a number in this range and answer the questions with 'y' for yes and 'n' for no." << endl;
 
         do {
-                int guess = (supValue + infValue) / 2;
-                cout << "Is your number smaller or equal to " << guess << endl;
+                int middleRange = (supValue + infValue) / 2;
+                cout << "Is your number smaller or equal to " << middleRange << endl;
                 cin >> userChoice;
 
                 if (userChoice == 'y') {
-                    supValue = guess;
+                    supValue = middleRange;
                 } else if (userChoice == 'n') {
-                    infValue = guess + 1;
+                    infValue = middleRange + 1;
                 } else {
                     cout << "Error, choose 'y' or 'n'" << endl;
                 }
@@ -90,20 +94,18 @@ int main() {
        
         char modeChoice;
 
-        cout << "For random mode enter 'r' and for in the middle mode enter 'm'" << endl;
+        cout << "For random mode enter 'r' and for in default mode enter 'd'" << endl;
         cin >> modeChoice;
+
+        int randomNumber = 0;
+            int seed = time(NULL);
+            //cout << "The seed is: " << seed << endl;
+            srand(seed);
 
         if (modeChoice == 'r') {
            
             cout << yesOrNo << endl;
            
-
-            
-            int randomNumber = 0;
-            int seed = time(NULL);
-            //cout << "The seed is: " << seed << endl;
-            srand(seed);
-            
             do {
             
                 //cout << "Je pense que le nombre est compris entre: " << infValue << " et " << supValue << " (inclus)" << endl;
@@ -127,30 +129,30 @@ int main() {
             
             cout << "Your number is: " << infValue << endl;
              
-        } else if (modeChoice == 'm') {
-            //in the middle mode code here
+        } else if (modeChoice == 'd') {
+            
+            do {
+                int middleRange = (supValue + infValue) / 2;
+                cout << "Is your number smaller or equal to " << middleRange << "?" << endl;
+
+                if (guessNumber <= middleRange) {
+                cout << "y" << endl;
+                supValue = middleRange;
+            } else if (guessNumber > middleRange) {
+                cout << "n" << endl;
+                infValue = middleRange + 1;
+            } else {
+                cout << "Error, choose 'y' or 'n'" << endl;
+                }
+            }
+            while (infValue != supValue);
+            
+            cout << "Your number is: " << infValue << endl;
         } else {
             cout << "Wrong letter, please choose between 'r' and 'm'." << endl;
         }
 
-        
-/*
-        do {
-            int guess = (supValue + infValue) / 2;
-            cout << "Is your number smaller or equal to " << guess << "?" << endl;
-            cin >> userChoice;
 
-            if (guessNumber > guess) {
-                userChoice == 'y';
-                infValue = guess;
-            } else if (guessNumber < guess) {
-                   userChoice == 'n';
-                   supValue = guess + 1;
-                 }
-        } while (infValue != supValue );
-
-        cout << "Your number is: " << infValue << endl;
-*/
     } else {
         cout << "Error!" << endl;
     }
